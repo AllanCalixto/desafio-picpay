1e9501d67c0f85f6a33b897289c72b1687fbddb5
Allan <allancalixto1@gmail.com>
2024-03-08 09:18:15 -0300

    feature: validation

--- src/main/java/br/com/allan/desafio/domain/user/UserDto.java
+++ src/main/java/br/com/allan/desafio/domain/user/UserDto.java
@@ -1,9 +1,19 @@
 package br.com.allan.desafio.domain.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record UserDto(
+		@NotBlank
 		String name,
+
+		@NotBlank
+		@Email
+
+		@NotBlank
 		String document, 
 		BigDecimal balance, 
 		String email,
